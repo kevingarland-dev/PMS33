24c6fb856ee53fa824ed4ca2083abe652dd5343f
Emmanuel Abaidoo <kevingarland020@gmail.com>
2026-08-12 11:14:39 +0000

    Replace sensitive information with placeholders
    Updated WiFi credentials and Adafruit IO key placeholders.

--- firmware/include/secrets.h
+++ firmware/include/secrets.h
@@ -3,11 +3,11 @@
 // ─── WiFi Credentials
 // ───────────────────────────────────────────────────────── Replace with your
 // actual WiFi SSID and password
-#define WIFI_SSID "Nazzi_s"
-#define WIFI_PASS "gggggggh"
+#define WIFI_SSID "Your Network name"
+#define WIFI_PASS "Your network Password"
 
 // ─── Adafruit IO Key
 // ────────────────────────────────────────────────────────── Get your key at:
 // https://io.adafruit.com  →  click "My Key" (yellow button)
-#define AIO_USERNAME "skinnyman_247"
-#define AIO_KEY "aio_QtEP22pKgcYPR49arNLQYoV5Xgyl"
+#define AIO_USERNAME "Adafruit Username"
+#define AIO_KEY "AdafruitIOKey"
